32b9b21acdf7746081ff8cb39193afacafdb8fdf
expert68 <1413786269@qq.com>
2018-06-05 15:48:35 +0800

    量化

--- day6/量化系统--开发.py
+++ day6/量化系统--开发.py
@@ -119,6 +119,8 @@ from abupy import ABuPickTimeExecute
 sell_factors = [sell_factor1]
 capital = AbuCapital(1000000,benchmark)
 order_pd1,action_pd1,_  = ABuPickTimeExecute.do_symbols_with_same_factors(['usTSLA'],benchmark,buy_factors,sell_factors,capital,show=True)
+ABuTradeExecute.apply_action_to_capital(capital,action_pd1,kl_pd_manager)
+capital.capital_pd.capital_blance.plot()
 
 """
 从图入门--3中可以看大，大多数的交易卖出因子都生效了，但是效果很不好，量化交易系统一般都会有止盈止损卖出策略，
@@ -143,6 +145,129 @@ class AbuFactorAtrNStop(AbuFactorSellBase):
         if 'stop_win_n' in kwargs:
             #设置止盈的ATR倍数
             self.stop_win_n = kwargs['stop_win_n']
+            self.sell_type_extra_win = '{}:stop_win={}'.format(self.__class__.__name__, self.stop_win_n)
+
+    def fit_day(self, today, orders):
+        for order in orders:
+            profit = today.close - order.buy_price
+            stop_base = today.atr21 + today.atr14
+
+            if hasattr(self,'stop_win_n') and profit > 0 \
+                and profit > self.stop_win_n * stop_base:
+                #满足止盈条件,卖出股票
+                self.sell_type_extra = self.sell_type_extra_win
+                order.fit_sell_order(int(today.key),self)
+
+            if hasattr(self, 'stop_loss_n') and profit < 0 \
+                    and profit < -self.stop_loss_n * stop_base:
+                # 满足止损条件,卖出股票
+                self.sell_type_extra = self.sell_type_extra_win
+                order.fit_sell_order(int(today.key), self)
+
+            self.sell_type_extra = self.sell_type_extra_loss
+            order.fit_sell_order(int(today.key),self)
+
+
+from abupy import AbuFactorAtrNStop
+#趋势耿总策略止盈要大于止损设置值，这里分别为0.3和3.0
+sell_factor2 = {'stop_loss_n':0.3,'stop_win_n':3.0,'class':AbuFactorAtrNStop}
+
+#两个卖出因子并行同时生效
+sell_factors = [sell_factor1,sell_factor2]
+capital = AbuCapital(1000000,benchmark)
+order_pd2,action_pd2,_  = ABuPickTimeExecute.do_symbols_with_same_factors(['usTSLA'],benchmark,buy_factors,sell_factors,capital,show=True)
+ABuTradeExecute.apply_action_to_capital(capital,action_pd2,kl_pd_manager)
+capital.capital_pd.capital_blance.plot()
+
+"""
+注意：
+    1、上面的很多拟合优化操作在实际应用中是不可取的，比如最后使用的AbuFactorAtrNstop使交易盈利，以及之后即将说明的另一些手段
+    使整体交易变好的做法，不应该因为某些特定股票或者特定交易修改参数或者添加因子等方式使结果变好，这样就是过拟合择时交易系统，在
+    后面将通过示例讲解如何挑选参数及因子的选择问题
+    2、ATR只是仓位控制以及止盈止损控制的手段之一，并不是所有仓位控制和止盈止损的方法都会用到ATR，应用ATR是因为这个概念比较好理解，
+    当股价涨幅过高的时候必然会下调，当股价跌幅过大的是偶必然会上涨，所以一定要有止盈和止损。
+"""
+
+'#################################################################################################################################################'
+
+"""
+滑点买入，卖出价格的确定及策略的实现
+    abu量化系统为非高频操作，所以生成的买入单子以及卖出单子产生的信号，均在下一个交易日执行，
+    这种策略框架适合个人投资者，并非和大型基金私募去拼杀设备的好坏、拼杀速度，个人投资者不要妄想可以使用统计套利进行高频交易，
+    比如跨市场期权套利，它们确实是最安全的套利方式，但所付出的高额成本也不是小资金能承受的，而且这种非高频操作也会有相当多的
+    好处，比如可以使用机器学习甚至深度学习技术融入策略中
+"""
+
+"""
+既然是第二天执行订单，那应该使用第二天的什么价格买入或者卖出呢？
+默认的策略都是使用当天的均价买入卖出，示例如下：
+"""
+from abupy import AbuSlippageBuyBase
+import numpy as np
+from abc import ABCMeta
+# g_open_dow_rate = 0.07
+import six
+
+#需要继承AbuSlippageBuyBase
+
+
+# class AbuSlippageBuyMean(AbuSlippageBuyBase):
+#     def fit_price(self):
+#         if (self.kl_pd_buy.open / self.kl_pd_buy.preClose) < (1-g_open_dow_rate):
+#             #开盘下跌g_open_down_rate以上，单子失效
+#              return np.inf
+#         #买入价格为当天价格
+#         self.buy_price = np.mean([self.kl_pd_buy['high'],self.kl_pd_buy['low']])
+#         return self.buy_price
+
+#AbuSlippageBuyBase的代码如下：
+# class AbuSlippageBuyBase(six.with_metaclass(ABCMeta,object)):
+#     def __init__(self,kl_pd_buy,factor_name):
+#         self.buy_price = np.inf
+#         self.kl_pd_buy = kl_pd_buy
+#         self.factor_name = factor_name
+#
+#     # @abstractmethod
+#     def fit_price(self):
+#         pass
+
+"""
+    卖出执行策略AbuSlippageSellMean基本与AbuSlippageBuyMean相同，当然也可以实现多种复杂的当日交易策略，
+设置限价单，市价单，获取当日的分时数据，再次进行策略分析执行操作，但是如果回测数量足够多的情况下，比如
+全市场回测，按照大数定理，这个均值执行其实是最好的模拟，而且简单、运行速度快
+    上面的代码使用了一个小策略，即设置g_open_down_rate为0.07,即当当天开盘价格直接下探7%时，放弃买单
+"""
+"""
+下面编写一个独立的Slippage(滑点)策略，且修改g_open_down_rate的值为0.02
+示例如下：
+"""
+from abupy import AbuSlippageBuyBase
+
+g_open_dow_rate = 0.02
+
+class AbuSlippageBuymean(AbuSlippageBuyBase):
+    def fit_price(self):
+        if (self.kl_pd_buy.open / self.kl_pd_buy.preClose) < (1-g_open_dow_rate):
+            #开盘下跌g_open_down_rate以上，单子失效
+             return np.inf
+        #买入价格为当天价格
+        self.buy_price = np.mean([self.kl_pd_buy['high'],self.kl_pd_buy['low']])
+        return self.buy_price
+
+"""
+通过buy_factors的字典对象中传入slippage便可以自行设置滑点类，由于上面的交易是60日突破产生的买单，
+所以我们只修改60日突破的字典对象，执行后可以看到结果如图开发--4所示，过滤了两个60日突破的买单
+"""
+#针对60日使用AbuSlippageBuyMean
+buy_factors2 = [{'slippage':AbuSlippageBuymean,'xd':60,'class':AbuFactorBuyBreak},{'xd':42,'class':AbuFactorBuyBreak}]
+
+capital = AbuCapital(1000000,benchmark)
+order_pd3,action_pd3,_ = ABuPickTimeExecute.do_symbols_with_same_factors(['usTSLA'],benchmark,buy_factors2,sell_factors,capital,show=True)
+#输出结果如图开发--5所示
+
+
+
+
 
 
 
